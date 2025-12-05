aa29e829d6da84037104b37c463651b17982eb4c
Sonny Piers <sonny@fastmail.net>
2025-12-05 22:07:32 +0100

    Fix background service
    Fix https://github.com/sonnyp/Eloquent/issues/46

--- src/application.js
+++ src/application.js
@@ -1,8 +1,7 @@
 import Gio from "gi://Gio";
 import Adw from "gi://Adw";
-import GLib from "gi://GLib";
 import Xdp from "gi://Xdp";
-import XdpGtk from "gi://XdpGtk4";
+// import XdpGtk from "gi://XdpGtk4";
 
 import Window from "./window.js";
 import About from "./about.js";
@@ -14,7 +13,9 @@ import "./style.css";
 const portal = new Xdp.Portal();
 
 export default function Application() {
-  portal.set_background_status("LanguageTool server running", null).catch(console.error);
+  portal
+    .set_background_status("LanguageTool server running", null)
+    .catch(console.error);
 
   const application = new Adw.Application({
     application_id: "re.sonny.Eloquent",
@@ -23,15 +24,24 @@ export default function Application() {
   application.hold();
 
   application.connect("activate", () => {
-    const {window} = Window({
+    // const { window } =
+    Window({
       application,
     });
 
     (async () => {
-      const parent = XdpGtk.parent_new_gtk(window);
-      const success = await portal.request_background(parent, "Run LanguageTool in the background to use with 3rd party integrations and make Eloquent faster.", ["re.sonny.Eloquent", "--gapplication-service"], Xdp.BackgroundFlags.AUTOSTART, null);
-      console.debug('request background succeeded', success);
-    })().catch(console.error)
+      // See https://github.com/sonnyp/Eloquent/issues/46
+      // const parent = XdpGtk.parent_new_gtk(window);
+      const parent = null;
+      const success = await portal.request_background(
+        parent,
+        "Run LanguageTool in the background to make Eloquent faster and use 3rd party integrations.",
+        ["re.sonny.Eloquent", "--gapplication-service"],
+        Xdp.BackgroundFlags.AUTOSTART,
+        null,
+      );
+      console.debug("request background succeeded", success);
+    })().catch(console.error);
   });
 
   application.connect("shutdown", () => {
